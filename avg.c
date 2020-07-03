fd276904b8e4cb509fd2f0d16bd6c8dcbff0b3ff
Nahian Raisa <67549194+076-NahianRaisa@users.noreply.github.com>
2020-07-03 21:26:53 +0600

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+
+   float a,b,average;
+
+   printf("enter the first  number\n");
+   scanf("%f",&a);
+   printf("enter the second  number\n");
+   scanf("%f",&b);
+
+
+   average = (a+b)/2;
+   printf("the average is%f =\n",average);
+
+
+   return 0;
+}
+
